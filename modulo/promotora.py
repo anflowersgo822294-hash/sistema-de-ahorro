a83dfe819711ddb51b334d82e6cf670ead5d7ba3
anflowersgo822294-hash <anflowersgo822294@gmail.com>
2025-11-28 06:13:03 -0600

    Update promotora.py

--- modulo/promotora.py
+++ modulo/promotora.py
@@ -7,7 +7,8 @@ def interfaz_promotora(user):
     st.subheader("Miembros supervisados")
     try:
         miembros = run_query("""
-            SELECT m.* FROM miembro m
+            SELECT m.* 
+            FROM miembro m
             JOIN grupo g ON m.id_grupo = g.id_grupo
             WHERE g.Distrito = (SELECT Distrito FROM promotora WHERE id_promotora=%s)
         """, (user.get("id_promotora"),))
@@ -18,7 +19,8 @@ def interfaz_promotora(user):
     st.subheader("Ahorros registrados")
     try:
         ahorros = run_query("""
-            SELECT a.id_ahorro, m.Nombre, a.Monto_actual, a.Fecha_actualizacion, a.Resto, a.Saldo_min_inicial, a.Total_de_ahorro
+            SELECT a.id_ahorro, m.Nombre, a.Monto_actual, a.Fecha_de_actualizacion, 
+                   a.Resto, a.Saldo_min_inicial, a.Total_de_ahorro
             FROM ahorro a
             JOIN miembro m ON a.id_miembro = m.id_miembro
         """)
@@ -29,8 +31,9 @@ def interfaz_promotora(user):
     st.subheader("Cierres de ciclo")
     try:
         cierres = run_query("""
-            SELECT c.id_cierre, m.Nombre, c.Saldo_final, c.Fecha, c.Fondo_total_grupo, c.Monto_a_retirar
-            FROM cierre c
+            SELECT c.id_cierre, m.Nombre, c.Saldo_final, c.Fecha, 
+                   c.Fondo_total_grupo, c.Monto_a_retirar
+            FROM cierre_de_ciclo c
             JOIN miembro m ON c.id_miembro = m.id_miembro
         """)
         st.dataframe(cierres)
