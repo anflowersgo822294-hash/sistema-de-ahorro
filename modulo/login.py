930d96a08d0091946f18bd20f48415390eaa413f
anflowersgo822294-hash <anflowersgo822294@gmail.com>
2025-11-28 00:47:01 -0600

    Update login.py

--- modulo/login.py
+++ modulo/login.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from .config.conexion import obtener_conexion  # ✅ import relativo
+from modulo.config.conexion import obtener_conexion  # ✅ import absoluto
 
 def verificar_usuario(usuario, contrasena):
     con = obtener_conexion()
