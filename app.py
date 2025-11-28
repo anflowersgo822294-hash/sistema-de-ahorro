30e15ef54f9bac7c6d7acab4968a782d89621dfd
anflowersgo822294-hash <anflowersgo822294@gmail.com>
2025-11-28 00:20:57 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,35 +1,9 @@
-import mysql.connector
-import os
 import streamlit as st
 from modulos.login import login
-login()
 
-# Obtener las credenciales de las variables de entorno (seguridad)
-host = os.getenv("DB_HOST", "bzd1eng5tfooqaj4orni-mysql.services.clever-cloud.com")
-user = os.getenv("DB_USER", "urjmaioxweivcu3l")
-password = os.getenv("DB_PASSWORD", "uKTWahQxnMw9xcKTzG5C")
-database = os.getenv("DB_NAME", "bzd1eng5tfooqaj4orni")
+def main():
+    st.set_page_config(page_title="Sistema de Ahorro", layout="centered")
+    login()
 
-# Intentar establecer la conexión
-try:
-    conn = mysql.connector.connect(
-        host=host,
-        user=user,
-        password=password,
-        database=database
-    )
-    
-    # Verificar si la conexión fue exitosa
-    if conn.is_connected():
-        st.success("Conexión exitosa a la base de datos MySQL.")
-    else:
-        st.error("No se pudo conectar a la base de datos.")
-        
-except mysql.connector.Error as err:
-    # Mostrar el error si la conexión falla
-    st.error(f"Error al conectar con la base de datos: {err}")
-
-finally:
-    if conn.is_connected():
-        # Cerrar la conexión
-        conn.close()
+if __name__ == "__main__":
+    main()
