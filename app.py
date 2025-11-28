1f67ccad9d2a01a1408ffd60a03170f33a7cc8f0
anflowersgo822294-hash <anflowersgo822294@gmail.com>
2025-11-28 00:49:05 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import streamlit as st
-
+from modulo.login import login   # 👈 importa la función login
 
 def main():
     st.set_page_config(page_title="Sistema de Ahorro", layout="centered")
-    login()
+    login()   # ahora sí existe
 
 if __name__ == "__main__":
     main()
+
     
